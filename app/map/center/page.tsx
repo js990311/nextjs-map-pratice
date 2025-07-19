ed39a88756f5f0ffc3a16146ef8a8ecf7f5c7967
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-07-19 23:08:58 +0900

    feat: map:setCenter

--- app/map/center/page.tsx
+++ app/map/center/page.tsx
@@ -1,12 +1,40 @@
+"use client"
+
 import Map from "@/components/map/map";
+import { forwardRef, useRef } from "react";
 
 export default function MapCenter(){
-    
+    const mapRef = useRef(null);
+
     return (
         <div>   
             <Map
+                ref={mapRef}
                 className="w-full h-[500px]"
             ></Map>
+            <div>
+            <button
+                onClick={()=>{
+                    mapRef.current?.setCenter({
+                        lat: "35.13",
+                        lng: "129.05"
+                    });
+                }}
+            >
+                부산
+            </button>
+            <button
+                onClick={()=>{
+                    mapRef.current?.setCenter({
+                        lat: "36.35",
+                        lng: "127.38"
+                    });
+                }}
+            >
+                대전
+            </button>
+
+            </div>
         </div>
     );
 }
--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,37 +1,52 @@
 "use client"
 
 import Script from "next/script";
-import { useRef, useState } from "react";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+
+type MapHandler = {
+    setCenter : (center: {lat: number, lng: number}) => void;
+}
 
 type MapProps = {
-    className ?: string;
+    className ?: string
 };
 
-export default function Map({className} : Readonly<MapProps>){
-    const mapRef = useRef<HTMLDivElement | null>(null);
-    const [mapState, setMapState] = useState<naver.maps.Map | null>(null);
+const Map = forwardRef<MapHandler, MapProps>(
+    function Map({className} : Readonly<MapProps>, ref){
+        const mapRef = useRef<HTMLDivElement | null>(null);
+        const [mapState, setMapState] = useState<naver.maps.Map | null>(null);
 
-    const createNaverMap = () => {
-        if(!mapRef.current || !window.naver) return;
-        // map 객체가 null이면 만들기
-        const {naver} = window;
+        /** 상위컴포넌트에서 사용할 메서드 제공 */
+        useImperativeHandle(ref, () => ({
+            setCenter: (center)=>{
+                mapState.setCenter(center);
+            }
+        }), [mapState]);
 
-        let map: naver.maps.Map = new naver.maps.Map(mapRef.current);
-        setMapState(map);
-    }
+        const createNaverMap = () => {
+            if(!mapRef.current || !window.naver) return;
+            // map 객체가 null이면 만들기
+            const {naver} = window;
 
-    return (
-        <div>
-            <Script
-                strategy="afterInteractive"
-                src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=drawing`}
-                onReady={() => {
-                    createNaverMap();
-                }}
-            />
-            <div id="map" className={className} ref={mapRef}>
+            let map: naver.maps.Map = new naver.maps.Map(mapRef.current);
+            setMapState(map);
+        }
+
+        return (
+            <div>
+                <Script
+                    strategy="afterInteractive"
+                    src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=drawing`}
+                    onReady={() => {
+                        createNaverMap();
+                    }}
+                />
+                <div id="map" className={className} ref={mapRef}>
+                </div>
+
             </div>
+        );
+    }
+);
 
-        </div>
-    )
-}
+export default Map;
